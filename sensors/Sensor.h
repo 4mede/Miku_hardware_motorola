LineageOS/android_hardware_motorola

sensors: Use soong select to support various configurations
Change-Id: I652471dbbbc89d0f0175aa46f310d7fc73d452ee

sensors/Sensor.h
@@ -129,6 +129,7 @@ const std::string kTsDoubleTapEnabledPath = kTsPath + "double_tap_enabled";
 
 constexpr int32_t SENSOR_TYPE_BASE = static_cast<int32_t>(SensorType::DEVICE_PRIVATE_BASE) + 100;
 
+#ifdef ENABLE_DOUBLE_TAP
 class DoubleTapSensor : public SysfsPollingOneShotSensor {
   public:
     DoubleTapSensor(int32_t sensorHandle, ISensorsEventCallback* callback)
@@ -137,10 +138,12 @@ class DoubleTapSensor : public SysfsPollingOneShotSensor {
               "Double Tap Sensor", "org.lineageos.sensor.double_tap",
               static_cast<SensorType>(SENSOR_TYPE_BASE + 1)) {}
 };
+#endif
 
 const std::string kTsUdfpsPressedPath = kTsPath + "udfps_pressed";
 const std::string kTsUdfpsEnabledPath = kTsPath + "udfps_enabled";
 
+#ifdef ENABLE_UPFPS
 class UdfpsSensor : public SysfsPollingOneShotSensor {
   public:
     UdfpsSensor(int32_t sensorHandle, ISensorsEventCallback* callback)
@@ -149,6 +152,7 @@ class UdfpsSensor : public SysfsPollingOneShotSensor {
               "UDFPS Sensor", "org.lineageos.sensor.udfps",
               static_cast<SensorType>(SENSOR_TYPE_BASE + 2)) {}
 };
+#endif
 
 }  // namespace implementation
 }  // namespace subhal

sensors/SensorsSubHal.cpp
@@ -33,8 +33,12 @@ using ::android::hardware::Void;
 using ::android::hardware::sensors::V2_0::implementation::ScopedWakelock;
 
 SensorsSubHal::SensorsSubHal() : mCallback(nullptr), mNextHandle(1) {
+#ifdef ENABLE_DOUBLE_TAP
     AddSensor<DoubleTapSensor>();
+#endif
+#ifdef ENABLE_UPFPS
     AddSensor<UdfpsSensor>();
+#endif
 }
 
 Return<void> SensorsSubHal::getSensorsList_2_1(ISensors::getSensorsList_2_1_cb _hidl_cb) {

4mede/Miku_hardware_motorola

sensors/Sensor.h
@@ -117,6 +117,7 @@ const std::string kTsPath = "/sys/class/touchscreen/primary/";
 
 constexpr int32_t SENSOR_TYPE_BASE = static_cast<int32_t>(SensorType::DEVICE_PRIVATE_BASE) + 100;
 
+#ifdef ENABLE_DOUBLE_TAP
 const std::string kTsDoubleTapPressedPath = kTsPath + "double_tap_pressed";
 const std::string kTsDoubleTapEnabledPath = kTsPath + "double_tap_enabled";
 
@@ -128,7 +129,9 @@ class DoubleTapSensor : public SysfsPollingOneShotSensor {
                                     "org.lineageos.sensor.double_tap",
                                     static_cast<SensorType>(SENSOR_TYPE_BASE + 1)) {}
 };
+#endif
 
+#ifdef ENABLE_UDFPS
 const std::string kTsUdfpsPressedPath = kTsPath + "udfps_pressed";
 const std::string kTsUdfpsEnabledPath = kTsPath + "udfps_enabled";
 
@@ -139,6 +142,7 @@ class UdfpsSensor : public SysfsPollingOneShotSensor {
                   sensorHandle, callback, kTsUdfpsPressedPath, kTsUdfpsEnabledPath, "UDFPS Sensor",
                   "org.lineageos.sensor.udfps", static_cast<SensorType>(SENSOR_TYPE_BASE + 2)) {}
 };
+#endif
 
 }  // namespace implementation
 }  // namespace subhal

sensors/SensorsSubHal.cpp
@@ -24,8 +24,12 @@ using ::android::hardware::Void;
 using ::android::hardware::sensors::V2_0::implementation::ScopedWakelock;
 
 SensorsSubHal::SensorsSubHal() : mCallback(nullptr), mNextHandle(1) {
+#ifdef ENABLE_DOUBLE_TAP
     AddSensor<DoubleTapSensor>();
+#endif
+#ifdef ENABLE_UDFPS
     AddSensor<UdfpsSensor>();
+#endif
 }
 
 Return<void> SensorsSubHal::getSensorsList_2_1(ISensors::getSensorsList_2_1_cb _hidl_cb) {